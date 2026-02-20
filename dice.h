6096db608fc74e089e20530b949bc8191ba41f98
Th3Av1at0r <77683463+Th3Av1at0r@users.noreply.github.com>
2026-02-19 21:20:00 -0600

    Update dice.h

--- dice.h
+++ dice.h
@@ -5,107 +5,111 @@ using namespace std;
 
 int roll(int d, int num_of_rolls, bool a_or_d)
 {
-	srand(time(NULL));
+    srand(time(NULL));
 
-	if (a_or_d == true)
-	{
-		return ((rand() % d) + 1);
-	}
-	else
-	{
-		int sum = 0;
+    if (a_or_d == true)
+    {
+        return ((rand() % d) + 1);
+    }
+    else
+    {
+        int sum = 0;
 
-		for (int i = 0; i < num_of_rolls; i++)
-		{
-			sum += ((rand() % d) + 1);
-		}
+        for (int i = 0; i < num_of_rolls; i++)
+        {
+            sum += ((rand() % d) + 1);
+        }
 
-		return sum;
-	}
+        return sum;
+    }
 }
 
 int roll_with_advantage(int d, int num_of_rolls)
 {
-	int roll_a;
-	int roll_b;
+    int roll_a;
+    int roll_b;
 
-	if (num_of_rolls == 1)
-	{
-		roll_a = roll(d, num_of_rolls, true);
-		roll_b = roll(d, num_of_rolls, true);
+    if (num_of_rolls == 1)
+    {
+        roll_a = roll(d, num_of_rolls, true);
+        roll_b = roll(d, num_of_rolls, true);
 
-		if (roll_a >= roll_b)
-		{
-			return roll_a;
-		}
-		else
-		{
-			return roll_b;
-		}
+        if (roll_a >= roll_b)
+        {
+            return roll_a;
+        }
+        else
+        {
+            return roll_b;
+        }
 
-	}
-	else
-	{
-		int sum = 0;
+    }
+    else
+    {
+        int sum = 0;
 
-		for (int i = 0; i < num_of_rolls; i++)
-		{
-			roll_a = roll(d, num_of_rolls, true);
-			roll_b = roll(d, num_of_rolls, true);
+        for (int i = 0; i < num_of_rolls; i++)
+        {
+            roll_a = roll(d, num_of_rolls, true);
+            roll_b = roll(d, num_of_rolls, true);
 
-			if (roll_a >= roll_b)
-			{
-				sum += roll_a;
-			}
-			else
-			{
-				sum += roll_b;
-			}
+            if (roll_a >= roll_b)
+            {
+                sum += roll_a;
+            }
+            else
+            {
+                sum += roll_b;
+            }
 
-			return sum;
-		}
-	}
+            return sum;
+        }
+    }
+    
+    return 999;
 }
 
 int roll_with_disadvantage(int d, int num_of_rolls)
 {
-	int roll_a;
-	int roll_b;
+    int roll_a;
+    int roll_b;
 
-	if (num_of_rolls == 1)
-	{
-		roll_a = roll(d, num_of_rolls, true);
-		roll_b = roll(d, num_of_rolls, true);
+    if (num_of_rolls == 1)
+    {
+        roll_a = roll(d, num_of_rolls, true);
+        roll_b = roll(d, num_of_rolls, true);
 
-		if (roll_a <= roll_b)
-		{
-			return roll_a;
-		}
-		else
-		{
-			return roll_b;
-		}
+        if (roll_a <= roll_b)
+        {
+            return roll_a;
+        }
+        else
+        {
+            return roll_b;
+        }
 
-	}
-	else
-	{
-		int sum = 0;
+    }
+    else
+    {
+        int sum = 0;
 
-		for (int i = 0; i < num_of_rolls; i++)
-		{
-			roll_a = roll(d, num_of_rolls, true);
-			roll_b = roll(d, num_of_rolls, true);
+        for (int i = 0; i < num_of_rolls; i++)
+        {
+            roll_a = roll(d, num_of_rolls, true);
+            roll_b = roll(d, num_of_rolls, true);
 
-			if (roll_a <= roll_b)
-			{
-				sum += roll_a;
-			}
-			else
-			{
-				sum += roll_b;
-			}
+            if (roll_a <= roll_b)
+            {
+                sum += roll_a;
+            }
+            else
+            {
+                sum += roll_b;
+            }
 
-			return sum;
-		}
-	}
+            return sum;
+        }
+    }
+    
+    return 999;
 }
